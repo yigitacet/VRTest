366bc33c5ab6ef33785aafd9808ae9ca1c797124
hasanhuseyinyay <ygtacet@gmail.com>
2022-12-06 14:46:15 +0300

    Hicaz model added to project.

--- VRGameTutorial/Assets/Constant/Constant.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class Constant
-{
-    public static readonly int Grip = Animator.StringToHash("Grip");
-    public static readonly int Trigger = Animator.StringToHash("Trigger");
-}
